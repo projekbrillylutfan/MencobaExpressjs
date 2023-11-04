4f116b8b3ccfab922d81f8f64ef06ddac16e535a
projekbrillylutfan <projekbrillylutfan@gmail.com>
2023-11-04 23:06:15 +0700

    push penambahan kodingan bagian put dan delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const listUsers = require("./data/listUser.json");
+const { request } = require("http");
 const app = express();
 app.use(express.json());
 
@@ -61,8 +62,7 @@ app.post("/api/users", (req, res) => {
       },
     };
     res.status(400).send(resGagal);
-  } 
-  else {
+  } else {
     const userToCreate = {
       id: listUsers[listUsers.length - 1].id + 1,
       name: payload.name,
@@ -84,9 +84,77 @@ app.post("/api/users", (req, res) => {
   }
 });
 
-app.put("/api/users/:id", (req, res) => {});
+app.put("/api/users/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  let payload = req.body;
 
-app.delete("/api/users/:id", (req, res) => {});
+  if (!payload.name) {
+    const resGagal = {
+      status: "EROR",
+      message: "erro 400 Bad Request - Nama harus disertakan",
+      data: {
+        users_update: null,
+      },
+    };
+    res.status(400).send(resGagal);
+  } else {
+    const userToUpdate = listUsers.find((user) => user.id === id);
+
+    if (userToUpdate) {
+      userToUpdate.name = payload.name;
+
+      const response = {
+        status: "OK",
+        message: "Sukses Untuk memperbarui data user",
+        data: {
+          users: userToUpdate,
+        },
+      };
+
+      fs.writeFileSync("./data/listUser.json", JSON.stringify(listUsers));
+
+      res.status(200).send(response);
+    } else {
+      const resGagal = {
+        status: "ERROR",
+        message: "404 Not Found - User tidak ditemukan",
+        data: {
+          users_update: null,
+        },
+      };
+      res.status(404).send(resGagal);
+    }
+  }
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const userIdDelete = listUsers.filter(
+    (user) => user.id !== parseInt(req.params.id)
+  );
+
+  const response = {
+    status: "OK",
+    message: "Berhasil Menghapus user",
+    data: {
+      users: listUsers.filter((user) => user.id !== parseInt(req.params.id)),
+    },
+  };
+
+  const resGagal = {
+    status: "EROR",
+    message: "erro 404 not found",
+    data: {
+      users: null,
+    },
+  };
+
+  if (userIdDelete !== -1) {
+    res.send(response);
+    fs.writeFileSync("./data/listUser.json", JSON.stringify(userIdDelete));
+  } else {
+    res.status(404).send(resGagal);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
